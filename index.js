5238468bb0f2ea3e72b78e718257cac61647e256
Tano Paul <tanopaul38@gmail.com>
2023-08-15 10:24:55 -0400

    completed search bar functionality

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon?limit=151';
 const url = 'https://pokeapi.co/api/v2/pokemon';
 const pokemonUl = document.querySelector('.pokemon-list');
 const search = document.querySelector('form');
-let allPokemon;
+let allPokemon = [];
 
 
 function renderPokedex(pokemon) {
+    
     let pokemonLi = document.createElement('li');
     let pokemonSpanName = document.createElement('span');
     let pokemonSpanNumber = document.createElement('span');
@@ -27,31 +28,37 @@ function renderPokedex(pokemon) {
 
 }
 
+
+
 search.addEventListener('keyup', (e) => {
     e.preventDefault()
-
-
-    console.log(e.key)
+    let iterator = e.target.value.length;
+    let searchArray = e.target.value.split('');
+    pokemonUl.innerHTML = '';
+    allPokemon.forEach(pokemon => {
+        let splitName = pokemon.name.split('', iterator);
+        if (splitName.join('') == searchArray.join('')) {
+            renderPokedex(pokemon)
+        }
+        
+    })
 })
-
-// function filteredPokemon(allPokemon, )
 
 
 
 fetch(pokemonUrl)
 .then(resp => resp.json())
 .then(data => {
-    // console.log(data)
     data.results.forEach(pokemon => {
         fetch(`${url}/${pokemon.name}`)
         .then(resp => resp.json())
         .then(data => {
             renderPokedex(data);
-            let allPokemon = data;
-            console.log(data)
+            allPokemon.push(data);
         })
     });
 
 })
+
 
 
